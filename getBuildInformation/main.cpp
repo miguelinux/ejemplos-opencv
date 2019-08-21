9c6b3e07ddfcc7e2e23a5aad22b4a542ed58a19d
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2019-08-20 19:18:02 -0500

    add getBuildInformation

---
+++ getBuildInformation/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) <year> <name of author>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <opencv2/opencv.hpp>
+
+int main (int argc, char *argv[])
+{
+  std::cout << cv::getBuildInformation() << std::endl;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
+
