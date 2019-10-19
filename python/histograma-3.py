da2b6d015c5fea6cb1f83c98075647ed5069c0db
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2019-10-19 11:27:09 -0500

    py: histogramas

---
+++ python/histograma-3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+'''
+Taken from : http://acodigo.blogspot.com/2017/08/histogramas-opencv-python.html
+'''
+
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('../resources/lena.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.equalizeHist(img)
+cv.imshow('Histogramas', img)
+
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+plt.plot(hist, color='gray' )
+
+plt.xlabel('intensidad de iluminacion')
+plt.ylabel('cantidad de pixeles')
+plt.show()
+
+cv.waitKey()
+cv.destroyAllWindows()
